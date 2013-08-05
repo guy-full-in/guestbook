3e1b4495d1b539a70d10a81d7736a7e9fb582e98
guy-full-in <guy-full-in@yandex.ru>
2013-08-05 20:39:33 +0400

    Add links to sign in and sign out

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+  	if user_signed_in?
+  	  @post=Post.new
+  	else
+  	  redirect_to user_session_path
+  	end
+  end
+
+  def create
+  	@post=Post.new
+    @post.author=current_user.email
+    @post.content=params[:post][:content]
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
+  def index
+  	@posts=Post.all
+  end
+end
